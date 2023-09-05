2869fe7ad19364a397c887be0cc2c35e9373fc0c
Julianna <juliannagoldbas@gmail.com>
2023-09-05 18:19:55 -0400

    updating anagrams.py

--- anagrams.py
+++ anagrams.py
@@ -22,6 +22,7 @@ Space: ~O(n+m) because we have to store the character-count objects
 """
 from collections import Counter # For anagrams2 function
 
+# Solution 1
 def anagrams(s1, s2):
     return char_count(s1) == char_count(s2)
 
@@ -35,11 +36,26 @@ def char_count(s):
 
     return count
 
-# Using Python built in method, Counter
+# Solution 2 using Python built in method, Counter
 def anagrams2(s1, s2):
     return Counter(s1) == Counter(s2)
+
+# Solution 3 (older)
+def anagrams3(s, t):
+    if (len(s) != len(t)):
+        return False
+
+    dict_s, dict_t = {}, {}
+
+    for el in s:
+        dict_s[el] = dict_s.get(el, 0) + 1  # if el is not in dict_s, put 0 for its value, otherwise add 1
+
+    for el in t:
+        dict_t[el] = dict_t.get(el, 0) + 1
+
+    return dict_s == dict_t
 
 print(char_count('catss'))
 print(anagrams('restful', 'fluster'))
 print(anagrams2('cats', 'tacos'))
-
+print(anagrams3("anagrams", "aaangrms"))
